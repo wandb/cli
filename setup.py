469f8ca0e1b1e1b4db2fb64c21d5e9096a388f19
Shawn Lewis <shlewis@gmail.com>
2018-02-16 14:16:24 -0800

    Version 0.5a5

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ test_requirements = [
 
 setup(
     name='wandb-testing',
-    version='0.5a4',
+    version='0.5a5',
     description="A CLI and library for interacting with the Weights and Biases API.",
     long_description=readme,
     author="Chris Van Pelt",
