9332ae02a44688c3c0dbe1154713a2093a38d749
Chris Van Pelt <vanpelt@gmail.com>
2019-09-14 18:50:08 -0700

    Made mod callable

--- wandb/ui/__init__.py
+++ wandb/ui/__init__.py
@@ -2,6 +2,7 @@ from functools import wraps
 from google.colab import output
 import json
 import IPython
+import sys
 
 CODE = {
     "default": """<>
@@ -23,32 +24,36 @@ CODE = {
 }
 
 
-def __call__(inp, out, options={}):
-    template = "default"
-    if "image" in inp:
-        if options.get("webcam"):
-            template = "webcam"
-        elif options.get("canvas"):
-            template = "draw"
-        else:
-            template = "image"
-    elif "audio" in inp:
-        template = "audio"
-    elif "text" in inp:
-        template = "text"
-    display(IPython.display.HTML('''
-    <div id="root"></div>
-    <script type="text/javascript">
-    window.initialScope = %s
-    </script>
-    <link href="https://cocky-kowalevski-373523.netlify.com/static/css/main.0d13eeef.chunk.css" rel="stylesheet">
-    <script src="https://cocky-kowalevski-373523.netlify.com/static/js/2.9ae5943a.chunk.js" />
-    <script src="https://cocky-kowalevski-373523.netlify.com/static/js/main.bd6e96d6.chunk.js" />
-    ''' % json.dumps({"code": CODE[template]})))
+class UI(object):
+    def __call__(self, inp, out, options={}):
+        template = "default"
+        if "image" in inp:
+            if options.get("webcam"):
+                template = "webcam"
+            elif options.get("canvas"):
+                template = "draw"
+            else:
+                template = "image"
+        elif "audio" in inp:
+            template = "audio"
+        elif "text" in inp:
+            template = "text"
+        display(IPython.display.HTML('''
+        <div id="root"></div>
+        <script type="text/javascript">
+        window.initialScope = %s
+        </script>
+        <link href="https://cocky-kowalevski-373523.netlify.com/static/css/main.0d13eeef.chunk.css" rel="stylesheet">
+        <script src="https://cocky-kowalevski-373523.netlify.com/static/js/2.9ae5943a.chunk.js" />
+        <script src="https://cocky-kowalevski-373523.netlify.com/static/js/main.bd6e96d6.chunk.js" />
+        ''' % json.dumps({"code": CODE[template]})))
 
-    def wrap(f):
-        @wraps(f)
-        def wrapper(*args, **kwargs):
-            return f(input, output, *args, **kwargs)
-        return wrapper
-    return wrap
+        def wrap(f):
+            @wraps(f)
+            def wrapper(*args, **kwargs):
+                return f(input, output, *args, **kwargs)
+            return wrapper
+        return wrap
+
+
+sys.modules[__name__] = UI()
