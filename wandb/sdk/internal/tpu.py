519bfe7d17b1babc287158c56f6f814bcaf8d7e0
Fariz Rahman <farizrahman4u@gmail.com>
2020-12-04 00:23:36 +0400

    threads

--- wandb/sdk/internal/tpu.py
+++ wandb/sdk/internal/tpu.py
@@ -1,8 +1,8 @@
 from __future__ import absolute_import
 
 import logging
-import multiprocessing
 import os
+import threading
 
 
 try:
@@ -16,6 +16,15 @@ except ImportError:
 
 logger = logging.getLogger(__name__)
 
+
+# def _tpu_profiler_process_entry():
+#     from tensorflow.python.distribute.cluster_resolver import tpu_cluster_resolver
+#     from tensorflow.python.profiler import profiler_client
+#     tpu = os.environ["TPU_NAME"]
+#     service_addr = tpu_cluster_resolver.TPUClusterResolver([tpu]).get_master()
+#     service_addr = service_addr.replace("grpc://", "").replace(":8470", ":8466")
+#     res = profiler_client.monitor(service_addr, duration_ms=100, level=2)
+    
 
 class TPUProfiler(object):
     def __init__(
@@ -47,13 +56,9 @@ class TPUProfiler(object):
         service_addr = service_addr.replace("grpc://", "").replace(":8470", ":8466")
         self.service_addr = service_addr
         self.duration_ms = duration_ms
-        self._manager = multiprocessing.Manager()
-        self._dict = self._manager.dict()
-        self._dict["tpu_utilization"] = 0.0
-        self._process = multiprocessing.Process(
-            target=self._loop, args=(self._dict), daemon=True
-        )
-        self._process.start()
+        self._tpu_utilization = 0.
+        self._thread = threading.Thread(target=self._loop, daemon=True)
+        self._stop = False
 
     def _get_tpu_utilization(self):
         # this call blocks for duration_ms milliseconds
@@ -62,24 +67,18 @@ class TPUProfiler(object):
         )
         return float(res.split("Utilization ")[1].split(": ")[1].split("%")[0])
 
-    def _loop(self, dict_):
-        while True:
+    def _loop(self):
+        while not self._stop:
             try:
-                dict_["tpu_utilization"] = self._get_tpu_utilization()
+                self._tpu_utilization = self._get_tpu_utilization()
             except Exception:
                 pass
 
     def get_tpu_utilization(self):
-        if self._process is None:
-            raise Exception("This TPUProfiler instance has been stopped.")
-        if not self._process.is_alive():
-            raise Exception("TPUProfiler background process shutdown!")
-        return self._dict["tpu_utilization"]
+        return self._tpu_utilization
 
     def stop(self):
-        if self._process and self._process.is_alive():
-            self._process.terminate()
-        self._process = None
+        self._stop = True
 
 
 def is_tpu_available():
